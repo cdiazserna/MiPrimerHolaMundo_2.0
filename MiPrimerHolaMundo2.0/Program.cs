de7e5b0c779ad8dc8cef06f2422519b0cc7c4e77
Carlor Jr Diaz <cjunior90@gmail.com>
2024-02-09 21:28:24 -0500

    Código de mi primer Hola Mundo

---
+++ MiPrimerHolaMundo2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiPrimerHolaMundo2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("s!");
+        }
+    }
+}
